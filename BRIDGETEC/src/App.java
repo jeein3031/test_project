018c453a4228aa7c9406be9bd7c9ca373d9042b5
oasis1418 <oasis1418@gmail.com>
2025-04-02 00:24:17 +0900

    test1

---
+++ BRIDGETEC/src/App.java
@@ -0,0 +1,11 @@
+import homework.StarPattern;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("== 과제 TEST 1: ");
+        StarPattern.add();
+        System.out.println("== 과제 TEST 2: ");
+        StarPattern.sum();
+        StarPattern.minus();
+    }
+}
---
+++ BRIDGETEC/src/homework/StarPattern.java
@@ -0,0 +1,30 @@
+package homework;
+
+public class StarPattern {
+    public static void add() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void sum() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void minus() {
+        for (int i = 10; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
