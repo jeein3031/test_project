588113f5713da616bbff8711e24c0ac581d90d49
jeein3031 <inven000@gmail.com>
2025-04-04 10:34:31 +0900

    Add files via upload

---
+++ jeeinstudy/test0402.java
@@ -0,0 +1,22 @@
+package jeeinstudy;
+
+public class test0402 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 문제1. 1부터 100까지 의 합산구하기
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= 100; i++)  {
+			sum += i;
+		}
+		 System.out.println("정답 =" + sum);
+
+				}
+	    	
+	     }
+
+
+
+
